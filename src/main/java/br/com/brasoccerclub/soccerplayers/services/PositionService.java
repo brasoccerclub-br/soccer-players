26da94104747f3b06b10a6ab592834c102f144c7
João Pedro <jpedrodaibello1999@outlook.com>
2020-12-04 11:34:31 -0300

    Tratamento de exceções

--- src/main/java/br/com/brasoccerclub/soccerplayers/services/PositionService.java
+++ src/main/java/br/com/brasoccerclub/soccerplayers/services/PositionService.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.com.brasoccerclub.soccerplayers.dto.PositionDTO;
 import br.com.brasoccerclub.soccerplayers.entities.Position;
@@ -16,6 +17,7 @@ public class PositionService {
 	@Autowired
 	private PositionRepository repository;
 	
+	@Transactional(readOnly = true)
 	public List<PositionDTO> findAll() {
 		List<Position> list = repository.findAll();
 		return list.stream().map(x -> new PositionDTO(x)).collect(Collectors.toList());
