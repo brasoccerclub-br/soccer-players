77480a846844c763ccf87adb7cdb723be0e68400
João Pedro <jpedrodaibello1999@outlook.com>
2020-12-04 12:35:16 -0300

    findAllPaged, findById, insert, update e delete de Position

--- src/main/java/br/com/brasoccerclub/soccerplayers/resources/PositionResource.java
+++ src/main/java/br/com/brasoccerclub/soccerplayers/resources/PositionResource.java
@@ -1,12 +1,22 @@
 package br.com.brasoccerclub.soccerplayers.resources;
 
-import java.util.List;
+import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.brasoccerclub.soccerplayers.dto.PositionDTO;
 import br.com.brasoccerclub.soccerplayers.services.PositionService;
@@ -19,8 +29,38 @@ public class PositionResource {
 	private PositionService service;
 	
 	@GetMapping
-	public ResponseEntity<List<PositionDTO>> findAll() {
-		List<PositionDTO> list = service.findAll();
+	public ResponseEntity<Page<PositionDTO>> findAll(@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction,
+			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy) {
+		
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Page<PositionDTO> list = service.findAllPaged(pageRequest);
 		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<PositionDTO> findById(@PathVariable Long id) {
+		PositionDTO dto = service.findById(id);
+		return ResponseEntity.ok().body(dto);
+	}
+
+	@PostMapping
+	public ResponseEntity<PositionDTO> insert(@RequestBody PositionDTO dto) {
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
+	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<PositionDTO> update(@PathVariable Long id, @RequestBody PositionDTO dto) {
+		dto = service.update(id, dto);
+		return ResponseEntity.ok().body(dto);
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<PositionDTO> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/br/com/brasoccerclub/soccerplayers/services/PositionService.java
+++ src/main/java/br/com/brasoccerclub/soccerplayers/services/PositionService.java
@@ -1,15 +1,19 @@
 package br.com.brasoccerclub.soccerplayers.services;
 
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import br.com.brasoccerclub.soccerplayers.dto.PositionDTO;
 import br.com.brasoccerclub.soccerplayers.entities.Position;
 import br.com.brasoccerclub.soccerplayers.repositories.PositionRepository;
+import br.com.brasoccerclub.soccerplayers.services.exceptions.EntityNotFoundException;
+import br.com.brasoccerclub.soccerplayers.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class PositionService {
@@ -18,8 +22,48 @@ public class PositionService {
 	private PositionRepository repository;
 	
 	@Transactional(readOnly = true)
-	public List<PositionDTO> findAll() {
-		List<Position> list = repository.findAll();
-		return list.stream().map(x -> new PositionDTO(x)).collect(Collectors.toList());
+	public Page<PositionDTO> findAllPaged(PageRequest pageRequest) {
+		Page<Position> list = repository.findAll(pageRequest);
+		return list.map(x -> new PositionDTO(x));
+	}
+	
+	@Transactional(readOnly = true)
+	public PositionDTO findById(Long id) {
+		Optional<Position> obj = repository.findById(id);
+		Position entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+		return new PositionDTO(entity);
+	}
+	
+	@Transactional
+	public PositionDTO insert(PositionDTO dto) {
+		Position entity = new Position();
+		copyDtoToEntity(dto, entity);
+		entity = repository.save(entity);
+		return new PositionDTO(entity);
+	}
+	
+	@Transactional
+	public PositionDTO update(Long id, PositionDTO dto) {
+		try {
+			Position entity = repository.getOne(id);
+			copyDtoToEntity(dto, entity);
+			entity = repository.save(entity);
+			return new PositionDTO(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found: " + id);
+		}
+	}
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found: " + id);
+		}
+	}
+	
+	private void copyDtoToEntity(PositionDTO dto, Position entity) {
+		entity.setName(dto.getName());
+		entity.setFieldArea(dto.getFieldArea());
 	}
 }
